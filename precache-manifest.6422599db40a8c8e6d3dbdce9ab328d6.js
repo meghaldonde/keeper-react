d9a7b8bda72045f67829bb74dfc90e41c8bf09bb
meghaldonde <meghal.donde@gmail.com>
2020-08-17 20:57:37 -0400

    Updates

---
+++ precache-manifest.6422599db40a8c8e6d3dbdce9ab328d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac95d3a473d8c922252bf1eff05a4d9d",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "e10bebbe376487cddb84",
+    "url": "/keeper-app/static/js/2.c7a8dc0a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/keeper-app/static/js/2.c7a8dc0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78375b4e3bed0e0d3d4b",
+    "url": "/keeper-app/static/js/main.f751072f.chunk.js"
+  },
+  {
+    "revision": "3f7ec364bc84954cf321",
+    "url": "/keeper-app/static/js/runtime-main.afaa4161.js"
+  }
+]);
